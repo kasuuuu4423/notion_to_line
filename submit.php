0b0237e316788ce3d5e6be367af665663a7b046f
shimizu <decode4423@gmail.com>
2025-01-06 12:14:47 +0900

    🎉

--- submit.php
+++ submit.php
@@ -1,25 +1,68 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/require.php';
 
 use GuzzleHttp\Client;
 
-$client = new Client();
+$client = new Client(
+    [\GuzzleHttp\RequestOptions::VERIFY => false]
+);
+
+// POSTデータのバリデーション
+$postData = json_decode(file_get_contents('php://input'), true) ?? [];
+
+
+// Notionのプロパティから値を取得
+$properties = $postData['data']['properties'];
+
+// 名前の取得
+$name = '名前なし';
+if (!empty($properties['Name']['title'][0]['plain_text'])) {
+    $name = $properties['Name']['title'][0]['plain_text'];
+}
+
+// IDの取得
+$id = 'ID未設定';
+if (!empty($properties['ID']['unique_id']['number'])) {
+    $id = 'ID-' . $properties['ID']['unique_id']['number'];
+}
+
+// ステータスの取得
+$status = 'ステータス未設定';
+if (!empty($properties['Status']['status']['name'])) {
+    $status = $properties['Status']['status']['name'];
+}
+
+$editor = '編集者不明';
+if (!empty($properties['LastEditedBy']['people'][0]['name'])) {
+    $editor = $properties['LastEditedBy']['people'][0]['name'];
+}
+
+$message = "$id 「 $name 」が
+「 $editor 」 さんによって
+「 $status 」に移動されました。";
+
+// デバッグログ
+error_log("Sending message: " . $message);
+error_log("POST data: " . print_r($postData, true));
+
 $pushMessageUrl = 'https://api.line.me/v2/bot/message/push';
 
 $res = $client->post($pushMessageUrl, [
     'headers' => [
-        'Authorization' => 'Bearer ' . getenv('LINE_SECRET'),
+        'Authorization' => 'Bearer ' . $_ENV['LINE_CHANNEL_ACCESS_TOKEN'],
         'Content-Type' => 'application/json',
     ],
     'json' => [
-        'to' => getenv('LINE_TO_ID'),
+        'to' => $_ENV['LINE_TO_ID'],
         'messages' => [
             [
                 'type' => 'text',
-                'text' => 'Hello, world!',
+                'text' => $message . "\n" . date('Y-m-d H:i:s'),
             ],
         ],
     ],
 ]);
 
-var_dump($res);
+// デバッグログ
+error_log("Response: " . $res->getBody());
+error_log("Response status: " . $res->getStatusCode());
